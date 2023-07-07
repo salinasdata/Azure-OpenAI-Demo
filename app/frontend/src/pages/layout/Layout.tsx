766255751850e12133eceae1092592bfbf62e0a9
adrianashymoniak <adrianashymoniak@gmail.com>
2023-07-07 15:48:42 +0300

    feat: init commit template project without data

---
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,34 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <nav>
+                        <ul className={styles.headerNavList}>
+                            <li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Chat
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Ask a question
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
